2ef055e1dd48057f5d721f75dc57ddac07033068
poverbec <paul.overeck95@gmail.com>
2026-01-30 15:40:12 +0100

    running mariadb as user

--- src/requirements/MariaDB/tools/MariaDB.sh
+++ src/requirements/MariaDB/tools/MariaDB.sh
@@ -9,13 +9,22 @@ export db_root_password=$(cat /run/secrets/db_root_password)
 export db_user_password=$(cat /run/secrets/db_user_password)
 export wp_admin_password=$(cat /run/secrets/wp_admin_password)
 
+
+mkdir -p /run/mysqld
+chown mysql:mysql /run/mysqld
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	mysql_install_db --user=mysql --datadir=/var/lib/mysql
 	chown -R mysql:mysql /var/lib/mysql
 fi
 
-# Start mysqld temporarily to configure users in background 
-mysqld_safe --datadir=/var/lib/mysql & MYSQL_PID=$!
+
+#kein eigenes error handling mit sql save, container soll crashen wenn mysql schiefgeht , und nicht nur mysql save erneut gestartet werden
+# soll als PID 1 ausgefuehrt werden 
+
+#run mariadb as user
+mysqld --datadir=/var/lib/mysql  --user=mysql &&
+MYSQL_PID=$! 	#hold pid of the last background process 
 
 until mysqladmin ping --silent; do
 	sleep 1
@@ -27,7 +36,6 @@ mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${db_user_password}';" 
 
 mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-echo "33333333333"
 mysql -e "CREATE USER IF NOT EXISTS '${WP_ADMIN_USER}'@'%'IDENTIFIED BY '${wp_admin_password}';"
 
 mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${WP_ADMIN_USER}'@'%';"
@@ -39,15 +47,17 @@ mysql -e "FLUSH PRIVILEGES" || echo "Failed on FLUSH PRIVILEGES"
 
 #just gill the backgrond process gracefully > sending error message ins dev/null niavana 
 #mysqladmin shutdown -u root -p${db_root_password} 2>/dev/null || true
-mysqladmin shutdown -u root -p${db_root_password} || true
-sleep 2
-
+mysqladmin shutdown -u root -p${db_root_password}
+wait $MYSQL_PID
 
 #production mode 
 #Safe wrapper with auto-restart, logging, proper shutdown
-exec mysqld_safe --datadir=/var/lib/mysql
+#exec mysqld_safe --datadir=/var/lib/mysql
 
+exec mysqld --datadir=/var/lib/mysql --user=mysql
 #make fclean does not work 
 #last errro 
 #mariadb  | ERROR 1064 (42000) at line 1: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'PRIVILEDGES ON 'mydb. * TO 'paul'@'%'' at line 1
 
+
+#so eigentlih 
--- src/requirements/WordPress/tools/wordpress.sh
+++ src/requirements/WordPress/tools/wordpress.sh
@@ -9,9 +9,12 @@ set -e
 #    sleep 2
 #done
 
+DB_USER_PASS=$(cat /run/secrets/db_user_password)
+
 echo "MariaDB is ready!"
 export WP_PATH="/usr/local/bin/wp"
-
+export WP_ROOT="/var/www/html"
+export WP_USER="www-data"
 
 if [ ! -f "$WP_PATH" ]; then
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -21,10 +24,41 @@ fi
 #check if wp-cli works
 wp --info --allow-root || echo "wp-cli check failed, continuing..."
 
+cd /var/www/html
 
-exec "$@"  # Runs: php-fpm8.4 -F
+
+if [ ! -f "wp-settings.php" ]; then
+    wp core download --allow-root
+    
+    # 4. Create wp-config.php
+    # Connect WordPress to MariaDB container
+    wp config create --allow-root \
+        --dbname=$MYSQL_DATABASE \
+        --dbuser=$MYSQL_USER \
+        --dbpass=$DB_USER_PASS\
+        --dbhost=mariadb:3306 \
+		--dbprefix="$WP_DB_PREFIX"
+
+    # 5. Install WordPress
+    wp core install --allow-root \
+        --url="poverbec.42.fr" \
+         --title="Inception" \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --skip-email
+fi
+
+chown -R www-data:www-data $WP_ROOT
+
+
+exec php-fpm8.4 -F
 
 # https://wp-cli.org/
 #curl -0 https://raw.githubusercontent.com/wp-cli/wp-cli/v2.12.0/utils/wp-completion.bash
 #source /FULL/PATH/TO/wp-completion.bash
 #source ~/.bash_profile
+
+
+## jetzt muss mnoch WP Core gedownloaded werden
+#	 WP user/admin soll von mariadb nebehmen 
