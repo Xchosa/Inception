e718c04ece581b960538063b2865889d9560c764
poverbec <paul.overbeck95@gmail.com>
2026-04-17 15:15:11 +0200

    fixed nginx

--- src/requirements/nginx/tools/nginx.sh
+++ src/requirements/nginx/tools/nginx.sh
@@ -3,7 +3,7 @@
 set -e
 
 
-envsubst '${DOMAIN_NAME}' < /etc/nginx/template/default.conf.template > /etc/nginx/conf.d/default.conf
-
+#envsubst '${DOMAIN_NAME}' < /etc/nginx/template/nginx.conf.template > /etc/nginx/template/nginx.conf
+envsubst '${DOMAIN_NAME}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
