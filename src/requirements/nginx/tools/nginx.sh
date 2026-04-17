58242340bdb428dd3534f5bc69f1800451d9bcd3
Paul Overbeck <paul.overbeck95@gmail.com>
2026-04-17 11:49:43 +0200

    domains loaded by env

--- src/requirements/nginx/tools/nginx.sh
+++ src/requirements/nginx/tools/nginx.sh
@@ -3,8 +3,7 @@
 set -e
 
 
-#envsubst '${DOMAIN_NAME}' < /etc/nginx/templates/default.conf.template \
-#  	> /etc/nginx/conf.d/default.conf
+envsubst '${DOMAIN_NAME}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 
 exec nginx -g "daemon off;"
